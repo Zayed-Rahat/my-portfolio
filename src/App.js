a6e7762895793a661ca778f1d9d2f24c20232aa5
Ahammed Zayed Uddin Rahat <azurahat129@gmail.com>
2021-06-14 16:52:37 +0600

    main

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { Switch, Route } from 'react-router-dom';
+import './App.scss';
+import NavBar from './Components/NavBar';
+import HomePage from './Pages/HomePage';
+import AboutPage from './Pages/AboutPage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="sidebar">
+        <NavBar />
+      </div>
+      <div className="main-content">
+        <div className="content">
+          <Switch>
+            <Route path="/" exact>
+              <HomePage />
+            </Route>
+            <Route path="/about" exact>
+              <AboutPage />
+            </Route>
+
+          </Switch>
+        </div>
+      </div>
+
+
     </div>
   );
 }
